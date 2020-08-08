c6b231d2a3f7e98270569e38f1c80b5bd0caa550
Tomasz Kostur <tomasz.kostur@gmail.com>
2020-08-08 08:35:57 +0200

    Check if i can have slice in struct
    Well yes and no. Slice is not owned type, so it is need to
    explicitly add some lifetime. And lifetime are sth that I don't
    understand for now.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use std::fmt;
 
 fn main() {
+    let mut discussion = DiscussionThread{topic: "bania u cygana"};
+    println!("doscussion: {:?}", discussion);
+    discussion.topic = "szaszlyk";
+    println!("doscussion: {:?}", discussion);
 
+    let mut ss = "some string slice";
+    println!("{}",ss);
+    ss = "another string slice";
+    println!("{}",ss);
+}
+
+fn check_how_implementation_of_display_works() -> () {
     let some_vector: Vec<String> = vec![String::from("qwe"), String::from("asd"), String::from("zxc")];
     println!("{:?}", some_vector);
     let first_user = User{name: String::from("Pawelek"),
@@ -9,6 +20,11 @@ fn main() {
                           messages: vec![String::from("kurde"), String::from("sorry"), String::from("zapomnialem")]};
     println!("Let's try print all at once, Debug:\n{:?}", first_user);
     println!("Here is non 'Debug' message:\n{}", first_user);
+}
+
+#[derive(Debug)]
+struct DiscussionThread<'a> {
+    topic: &'a str
 }
 
 #[derive(Debug)]
