0503945c0fc6d0072031a7e04f3b8faca72fa273
Tomasz Kostur <tomasz.kostur@gmail.com>
2020-08-30 15:32:10 +0200

    Remove dead code

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,7 @@ mod socket_check {
     mod client {
         use std::io::prelude::*;
         pub fn do_the_clients_job() -> () {
-            //let mut stream = std::net::TcpStream::connect("127.0.0.1:12123").unwrap();
-            //let some_tytes = b"huehe bytes";
-            //stream.write(&[1]).unwrap()
             let mut stream = std::net::TcpStream::connect("127.0.0.1:34254").unwrap();
-
             stream.write_all(b"some_bytes").unwrap();
         }
     }
@@ -45,53 +41,4 @@ mod socket_check {
         }
     }
 
-}
-
-#[allow(dead_code)]
-mod first_experiments {
-    use std::fmt;
-    fn check_disscussion_thread_with_str_slice_with_lifetime() {
-        let mut discussion = DiscussionThread{topic: "bania bania"};
-        println!("doscussion: {:?}", discussion);
-        discussion.topic = "szaszlyk";
-        println!("doscussion: {:?}", discussion);
-    
-        let mut ss = "some string slice";
-        println!("{}",ss);
-        ss = "another string slice";
-        println!("{}",ss);
-    }
-    
-    fn check_how_implementation_of_display_works() -> () {
-        let some_vector: Vec<String> = vec![String::from("qwe"), String::from("asd"), String::from("zxc")];
-        println!("{:?}", some_vector);
-        let first_user = User{name: String::from("Pawelek"),
-                            surname: String::from("Ziesasecki"),
-                            messages: vec![String::from("kurde"), String::from("sorry"), String::from("zapomnialem")]};
-        println!("Let's try print all at once, Debug:\n{:?}", first_user);
-        println!("Here is non 'Debug' message:\n{}", first_user);
-    }
-    
-    #[derive(Debug)]
-    struct DiscussionThread<'a> {
-        topic: &'a str
-    }
-    
-    #[derive(Debug)]
-    struct User {
-        name: String,
-        surname: String,
-        messages: Vec<String>
-    }
-    
-    impl fmt::Display for User {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            let mut s = fmt::format(format_args!("{} {}\nPowiedzial:\n", self.name, self.surname));
-            for message in self.messages.iter() {
-                s.push_str(message);
-                s.push_str("\n");
-            }
-            write!(f, "{}\n", s)
-        }
-    }
 }
