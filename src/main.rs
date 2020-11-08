ae8c630b1b11f46a7227c02555bca051a5adde91
Tomasz Kostur <tomasz.kostur@gmail.com>
2020-11-08 12:28:56 +0100

    Just drop old code

--- src/main.rs
+++ src/main.rs
@@ -2,90 +2,11 @@ extern crate rmp_serde;
 
 fn main() {
     println!("heavy refactor, use tests instead");
-    //env_logger::init();
-    //let mode = std::env::args().nth(1).expect("You should specify '--client' or '--server' mode.");
-    //match mode.as_str() {
-    //    "--client" => communicator::do_the_job(communicator::Mode::Client),
-    //    "--server" => communicator::do_the_job(communicator::Mode::Server),
-    //    _ => panic!("Wrong arguments"),
-    //}
 }
 
 mod communicator {
-    //pub enum Mode {Client, Server}
-    //const IP_ADDR_AND_PORT: &str = "127.0.0.1:34254";
-
-    //pub fn do_the_job(mode: Mode) -> () {
-    //    match mode {
-    //        Mode::Client => client::do_the_clients_job(),
-    //        Mode::Server => server::start_server(),
-    //    }
-    //}
-
-    //mod data_structure {
-    //    use serde::{Serialize, Deserialize};
-//
-    //    #[derive(Debug, Serialize, Deserialize)]
-    //    pub struct User {
-    //        pub name: String,
-    //    }
-//
-    //    #[derive(Debug, Serialize, Deserialize)]
-    //    pub struct Message  {
-    //        pub autor: User,
-    //        pub data: String
-    //    }
-//
-    //    #[derive(Debug, Serialize, Deserialize)]
-    //    pub struct Topic {
-    //        pub name: String,
-    //        pub messages: Vec<Message>
-    //    }
-    //}
-
-    //pub fn dev_topic() -> data_structure::Topic {
-    //    use data_structure::*;
-    //    let u1 = User{name: String::from("Pawel")};
-    //    let u2 = User{name: String::from("Tomek")};
-    //    let m1 = Message{autor: u1, data: String::from("Some example message")};
-    //    let m2 = Message{autor: u2, data: String::from("Other example message")};
-    //    let t1 = Topic{name: String::from("Example topic"), messages: vec![m1, m2]};
-    //    t1
-    //}
-//
-    //pub mod client {
-    //    use std::io::Write;
-    //    use std::net::TcpStream;
-//
-    //    pub struct Client {
-    //        //ip_addr_and_port_: String,
-    //        stream_to_server_: TcpStream,
-    //    }
-//
-    //    pub fn create(ip_and_port: String) -> Client {
-    //        let  stream = TcpStream::connect(ip_and_port).expect("Couldnt connect to message_server");
-    //        Client{/*ip_addr_and_port_: ip_and_port, */stream_to_server_: stream}
-    //    }
-//
-    //    impl Client {
-    //        pub fn dummy_method(&self) {
-    //            log::debug!("from Client's dummy method, my ip and port is");
-    //        }
-    //    }
-//
-    //    pub fn do_the_clients_job() -> () {
-    //        let mut stream = std::net::TcpStream::connect(super::IP_ADDR_AND_PORT).unwrap();
-    //    //    stream.write_all(b"some_bytes").unwrap();
-    //        let example = super::dev_topic();
-    //        let message_pack_buffer: Vec<u8> = rmp_serde::to_vec(&example).unwrap();
-    //        let bytes_send = stream.write(&message_pack_buffer).unwrap();
-    //        log::info!("{} was send to server", bytes_send);
-    //    }
-    //
-    //}
 
     pub mod server {
-        //use super::data_structure::*;
         use std::io::Read;
         use std::net::{TcpListener, TcpStream};
         use std::sync::mpsc;
@@ -129,7 +50,7 @@ mod communicator {
         impl TopicWorker {
             fn topic_loop(&self) {
                 loop {
-                    println!("loop iter");
+                    //println!("loop iter");
                     let command = self.command_receiver.try_recv();
                     match command {
                         Ok(command) => {
@@ -144,25 +65,18 @@ mod communicator {
                 }
             }
             fn regular_msg_receieve(&self)  {
-                println!("from regular_msg_receieve");
-                //let (mut stream, _addr) = self.message_listener.accept().expect("Couldn't accept incoming connection");
+                //println!("from regular_msg_receieve");
                 let stream  = self.message_listener.accept();
                 match stream {
                     Ok(s) => {self.handle_incoming_message(s.0)},
-                    Err(e) if e.kind()== ErrorKind::WouldBlock => { println!("would block") },
+                    Err(e) if e.kind()== ErrorKind::WouldBlock => { (/* just do nothing and wait for another loop*/ ) },
                     Err(e) => panic!("encountered IO error: {}", e)
                     }
-                //log::info!("stream {:?} was accepted", stream);
-                //let mut income_buffer: Vec<u8> = Vec::new();
-                //let bytes_read = stream.read(&mut income_buffer).unwrap(); // why read_to_end instead just read?
-                //println!("{} bytes was read", bytes_read);
-                //let message: Topic = rmp_serde::from_slice(&income_buffer).unwrap();
-                //log::info!("send message is: {:?}", message);
             }
             fn command_handle(&self, command: InterfaceCommands) {
                 match command {
                     InterfaceCommands::Start => self.handle_start_command(),
-                    InterfaceCommands::Stop => panic!("Unexpected match to command handler"),
+                    InterfaceCommands::Stop => panic!("Unexpected match to command handler"), // the "Stop" command is allready handled in topic loop
                 }
             }
             fn handle_start_command(&self) {
@@ -176,52 +90,6 @@ mod communicator {
             }
 
         }
-        ///////////////////////
-
-        //pub struct Server {
-        //    server_listener_: TcpListener,
-        //}
-
-        //pub fn create(ip_and_port: String) -> Server {
-        //    let listener = std::net::TcpListener::bind(ip_and_port).expect("Cannot create Server");
-        //    Server{server_listener_: listener, listening_: false}
-        //}
-
-        //impl Server {
-        //    pub fn listen_forever(self) {
-        //        let listening_closure = move || {
-        //            while self.listening_{
-        //                let (mut stream, _addr) = self.server_listener_.accept().expect("Couldn't accept incoming connection");
-        //                //let mut stream =  stream.unwrap();
-        //                log::info!("stream {:?} was accepted", stream);
-        //                let mut income_buffer: Vec<u8> = Vec::new();
-        //                let bytes_read = stream.read_to_end(&mut income_buffer).unwrap(); // why read_to_end instead just read?
-        //                log::info!("{} bytes was read", bytes_read);
-        //                let message: Topic = rmp_serde::from_slice(&income_buffer).unwrap();
-        //                log::info!("send message is: {:?}", message);
-        //            }
-        //        };
-        //        std::thread::spawn(listening_closure);
-        //    }
-        //    pub fn stop_listening(&mut self) {
-        //        self.listening_ = false;
-        //    }
-        //}
-//
-        //pub fn start_server() {
-        //    log::info!("Server started");
-        //    let listener = std::net::TcpListener::bind(super::IP_ADDR_AND_PORT).unwrap();
-        //    for stream in listener.incoming() {
-        //        let mut stream =  stream.unwrap();
-        //        log::info!("stream {:?} was accepted", stream);
-        //        let mut income_buffer: Vec<u8> = Vec::new();
-        //        let bytes_read = stream.read_to_end(&mut income_buffer).unwrap(); // why read_to_end instead just read?
-        //        log::info!("{} bytes was read", bytes_read);
-        //        let message: Topic = rmp_serde::from_slice(&income_buffer).unwrap();
-        //        log::info!("send message is: {:?}", message);
-        //    }
-        //    log::info!("Server stopped");
-        //}
     }
 }
 
@@ -241,61 +109,6 @@ mod tests {
         let mut stream = std::net::TcpStream::connect(IP_ADDR_AND_PORT).unwrap();
         stream.write_all(b"some_bytes").unwrap();
         thread::sleep(Duration::from_secs(1));
-        //let example = super::dev_topic();
-        //let message_pack_buffer: Vec<u8> = rmp_serde::to_vec(&example).unwrap();
-        //let bytes_send = stream.write(&message_pack_buffer).unwrap();
-        //log::info!("{} was send to server", bytes_send);
-
         topic.stop();
     }
-
-    //#[test]
-    //fn test_basic_communication(){
-    //    initialize_logger();
-    //    start_server_thread();
-    //    std::thread::sleep(std::time::Duration::from_millis(20)); // Sleep to be sure that server is allready up
-    //    do_the_job(Mode::Client);
-    //}
-
-    //#[test]
-    //fn test_client_impl(){
-    //    initialize_logger();
-    //    start_server_thread();
-    //    let client_instance = client::create(String::from(IP_ADDR_AND_PORT));
-    //    client_instance.dummy_method();
-    //}
-
-    //#[test]
-    //fn test_start_server() {
-    //    let server_instance = server::create(String::from(IP_ADDR_AND_PORT));
-    //    let instance_ref = &server_instance;
-    //    std::thread::spawn(|| {se.listen_forever()});
-    //    std::thread::sleep(std::time::Duration::from_millis(200));
-    //    instance_ref.stop_listening();
-    //}
-//
-    //// test utils
-    //fn start_server_thread() {
-    //    std::thread::spawn( || {
-    //        do_the_job(Mode::Server);
-    //    });
-    //    std::thread::sleep(std::time::Duration::from_millis(200));
-    //}
-//
-    //fn initialize_logger() {
-    //    let _= env_logger::try_init();
-    //}
-
-    //struct Test_Environment{
-    //    logger_initialized: bool
-    //}
-    //impl Test_Environment {
-    //    pub fn initialize_logger(&mut self) {
-    //        if !self.logger_initialized {
-    //            env_logger::init();
-    //            self.logger_initialized = true;
-    //        }
-    //    }
-    //}
-
 }
