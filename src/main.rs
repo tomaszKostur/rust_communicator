6b22e836fd4c56dd834150d029a09ef2b0e5783e
Tomasz Kostur <tomasz.kostur@gmail.com>
2020-11-08 22:03:24 +0100

    Use Message structure with serialization

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ fn main() {
 }
 
 mod communicator {
+    use serde::{Serialize, Deserialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct Message {
+        pub autor: String,
+        pub topic: String,
+        pub message: String,
+    }
 
     pub mod server {
         use std::io::Read;
@@ -13,6 +21,7 @@ mod communicator {
         use std::thread;
         use std::time::Duration;
         use std::io::ErrorKind;
+        use super::Message;
 
         pub struct Interface {
             topic_thread: std::thread::JoinHandle<()>,
@@ -36,7 +45,7 @@ mod communicator {
                 let (tx, rx) =  mpsc::channel::<InterfaceCommands>();
                 let listener = std::net::TcpListener::bind(ip_and_port).expect("Cannot create TcpListener");
                 listener.set_nonblocking(true).expect("Cannot set nonblocking listener");
-                let topic_thread = TopicWorker{command_receiver: rx, message_listener: listener};
+                let mut topic_thread = TopicWorker{command_receiver: rx, message_listener: listener, message_container: vec!()};
                 let worker_loop = move || {topic_thread.topic_loop()};
                 let topic_thread = thread::spawn(worker_loop);
                 Interface{topic_thread: topic_thread, command_pipe: tx}
@@ -46,9 +55,10 @@ mod communicator {
         struct TopicWorker {
             command_receiver: mpsc::Receiver<InterfaceCommands>,
             message_listener: TcpListener,
+            message_container: Vec<Message>,
         }
         impl TopicWorker {
-            fn topic_loop(&self) {
+            fn topic_loop(&mut self) {
                 loop {
                     //println!("loop iter");
                     let command = self.command_receiver.try_recv();
@@ -64,7 +74,7 @@ mod communicator {
                     thread::sleep(Duration::from_millis(100)); // performance save
                 }
             }
-            fn regular_msg_receieve(&self)  {
+            fn regular_msg_receieve(&mut self)  {
                 //println!("from regular_msg_receieve");
                 let stream  = self.message_listener.accept();
                 match stream {
@@ -82,11 +92,16 @@ mod communicator {
             fn handle_start_command(&self) {
                 println!("start command handler");
             }
-            fn handle_incoming_message(&self, mut stream: TcpStream) {
+            fn handle_incoming_message(&mut self, mut stream: TcpStream) {
                 println!("handle_incoming_message");
                 let mut buf: Vec<u8> = Vec::new();
-                stream.read(&mut buf);
-                println!("after read");
+                stream.read(&mut buf).expect("Cannot read incoming message from socket");
+                println!("readed buffer is: {:?}", buf);
+                if buf.len() != 0{
+                    let message: Message = rmp_serde::from_slice(&buf).expect("Cannot deserialize message");
+                    println!("Deserialized message is: {:?}", message);
+                    self.message_container.push(message);
+                }
             }
 
         }
@@ -107,8 +122,25 @@ mod tests {
         topic.start();
 
         let mut stream = std::net::TcpStream::connect(IP_ADDR_AND_PORT).unwrap();
-        stream.write_all(b"some_bytes").unwrap();
+
+        let test_msg = Message{autor: String::from("Pawel Z"),
+                               topic: String::from("Total chaos"),
+                               message: String::from("Sorry for being late!"),
+                            };
+        let test_msg: Vec<u8> = rmp_serde::to_vec(&test_msg).expect("Cannot serialize test_msg");
+        stream.write_all(&test_msg).expect("Cannot write to socket");
         thread::sleep(Duration::from_secs(1));
         topic.stop();
+    }
+
+    #[test]
+    fn test_serialize() {
+        let test_msg = Message{autor: String::from("Pawel Z"),
+                               topic: String::from("Total chaos"),
+                               message: String::from("Sorry for being late!"),
+                            };
+        let test_msg: Vec<u8> = rmp_serde::to_vec(&test_msg).expect("Cannot serialize test_msg");
+        let test_msg: Message = rmp_serde::from_slice(&test_msg).unwrap();
+        println!("{:?}", test_msg);
     }
 }
